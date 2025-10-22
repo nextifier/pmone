61c837a4c6a4b582d466782b4784493ba2a28a62
Nextifier <nextifier@gmail.com>
2025-10-22 19:01:14 +0700

    updates

--- frontend/app/components/AppSidebarNavProjects.vue
+++ frontend/app/components/AppSidebarNavProjects.vue
@@ -4,24 +4,36 @@
       projects?.length && user?.roles?.some((role) => ['master', 'admin', 'staff'].includes(role))
     "
   >
-    <SidebarGroupLabel class="text-muted-foreground tracking-tight">Projects</SidebarGroupLabel>
-    <SidebarMenu>
-      <div v-for="project in projects" :key="project?.id" class="tracking-tight">
-        <NuxtLink
-          :to="`/projects/${project?.username}/edit`"
-          @click="setOpenMobile(false)"
-          activeClass="*:bg-muted"
-        >
-          <SidebarMenuButton :tooltip="project?.name">
-            <Avatar
-              :model="project"
-              class="size-6 shrink-0 group-data-[state=collapsed]:size-4.5 group-data-[state=collapsed]:scale-150"
-            />
-            <span class="line-clamp-1 text-sm tracking-tight">{{ project?.name }}</span>
-          </SidebarMenuButton>
-        </NuxtLink>
-      </div>
-    </SidebarMenu>
+    <Collapsible as-child :default-open="true" class="group/collapsible">
+      <CollapsibleTrigger as-child>
+        <SidebarGroupLabel class="text-muted-foreground hover:bg-muted mb-1 tracking-tight">
+          <span>Projects</span>
+          <Icon
+            name="lucide:chevron-right"
+            class="ml-auto size-4 transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90"
+          />
+        </SidebarGroupLabel>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <SidebarMenu class="gap-y-1.5">
+          <div v-for="project in projects" :key="project?.id" class="tracking-tight">
+            <NuxtLink
+              :to="`/projects/${project?.username}/edit`"
+              @click="setOpenMobile(false)"
+              activeClass="*:bg-muted"
+            >
+              <SidebarMenuButton :tooltip="project?.name">
+                <Avatar
+                  :model="project"
+                  class="size-6 shrink-0 group-data-[state=collapsed]:size-4.5 group-data-[state=collapsed]:scale-150"
+                />
+                <span class="line-clamp-1 text-sm tracking-tight">{{ project?.name }}</span>
+              </SidebarMenuButton>
+            </NuxtLink>
+          </div>
+        </SidebarMenu>
+      </CollapsibleContent>
+    </Collapsible>
   </SidebarGroup>
 </template>
 
