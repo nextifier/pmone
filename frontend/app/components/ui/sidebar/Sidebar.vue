4fef4242451dbb80e5574d5bce782018ae4f24a2
Nextifier <nextifier@gmail.com>
2026-01-28 13:55:40 +0700

    updates

--- frontend/app/components/ui/sidebar/Sidebar.vue
+++ frontend/app/components/ui/sidebar/Sidebar.vue
@@ -87,7 +87,7 @@ watch(isSwiping, () => {
           'fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear lg:flex',
           side === 'left'
             ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
-            : 'right-[calc(0+var(--sidebar-width))] group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
+            : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
           // Adjust the padding for floating and inset variants.
           variant === 'floating' || variant === 'inset'
             ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
