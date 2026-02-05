fd62e1f1c02c8955fb39118878faed4bc22aefb1
Nextifier <nextifier@gmail.com>
2026-02-06 01:11:57 +0700

    updates

--- frontend/app/components/inbox/InboxTableItem.vue
+++ frontend/app/components/inbox/InboxTableItem.vue
@@ -10,19 +10,19 @@
       </NuxtLink>
     </div>
 
-    <div class="text-muted-foreground flex flex-wrap items-center gap-x-2 text-sm tracking-tight">
+    <div class="text-muted-foreground flex items-center gap-x-2 text-sm tracking-tight">
       <FlagComponent
         v-if="countryCode"
         v-tippy="countryName"
         :country="countryCode"
         class="h-3! shrink-0 rounded-xs! shadow-sm"
       />
-      <div class="inline">
+      <div class="inline truncate">
         <span v-if="submission.form_data_preview?.name">
           {{ submission.form_data_preview.name }}
         </span>
         <span v-if="submission.form_data_preview?.brand_name">
-          - {{ submission.form_data_preview.brand_name }}
+          - Brand: {{ submission.form_data_preview.brand_name }}
         </span>
         <!-- <span v-if="submission.form_data_preview?.email" class="text-muted-foreground">
           - {{ submission.form_data_preview.email }}
